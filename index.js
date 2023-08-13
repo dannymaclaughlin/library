2972af861a64a9028763efe26b787afa0d691a2a
dannymaclaughlin <dannymaclaughlin@gmail.com>
2023-08-13 14:57:09 -0700

    Changed variable names to be consistent.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // -  All of your book objects are going to be stored in a simple array,
 //    so add a function to the script (not the constructor) that:
-//        - can take user’s input
-//        - and store the new book objects into an array.
+//        [x] can take user’s input and
+//        [ ] store the new book objects into an array.
 
+// book.info should be written out as:
 // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 
 let myLibrary = [];
@@ -18,11 +19,11 @@ function Book(title, author, numOfPages, readStatus) {
 }
 
 function addBookToLibrary() {
-    bookTitle = prompt("Book Title");
-    bookAuthor = prompt("Book Author");
-    bookPages = prompt("Number of Book Pages");
-    bookReadStatus = prompt("Have you read this book yet? yes/no");
+    title = prompt("Book Title");
+    author = prompt("Book Author");
+    numOfPages = prompt("Number of Book Pages");
+    readStatus = prompt("Have you read this book yet? yes/no");
 
-    const book = new Book(bookTitle, bookAuthor, bookPages, bookReadStatus);
+    const book = new Book(title, author, numOfPages, readStatus);
     console.log(book);
 }
