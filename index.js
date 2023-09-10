a35ebb98074b7f5be51328c24677b1ec5cf3f0c7
dannymaclaughlin <dannymaclaughlin@gmail.com>
2023-09-09 17:09:40 -0700

    "Add New Book" button will bring up dialog modal. This modal still needs to be updated to show a form.

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ let myLibrary = [
         "read status": "no",
     },
     {
-        "title": "potatoes vol. 2",
+        "title": "potatoes vol. 3",
         "author": "steve",
         "number of pages": 2,
         "read status": "no",
     },
     {
-        "title": "potatoes vol. 2",
+        "title": "potatoes vol. 4",
         "author": "steve",
         "number of pages": 2,
         "read status": "no",
     },
     {
-        "title": "potatoes vol. 2",
+        "title": "potatoes vol. 5",
         "author": "steve",
         "number of pages": 2,
         "read status": "no",
@@ -51,17 +51,21 @@ function addBookToLibrary(title, author, numOfPages, readStatus) {
 //     loop through the array
 //         display each book object on the page
 function displayBook() {
+    // grabs the [data-book-placeholder] element from the HTML file and assigns it to...
+    // ...the variable "bookPlaceholder".
     let bookPlaceholder = document.querySelector('[data-book-placeholder]');
     bookPlaceholder.innerHTML = '';
+    // bookPlaceholder styling
     bookPlaceholder.style.display = "grid";
     bookPlaceholder.style.gridTemplateColumns = "1fr 1fr 1fr 1fr";
     bookPlaceholder.style.height = "fit-content";
+    bookPlaceholder.style.border = "3px solid pink";
     
     for (let i = 0; i < myLibrary.length; i++) {
         const book = myLibrary[i];
         const bookElement = document.createElement('div');
-        // border toggle for bookElement
-        bookElement.style.border = "1px solid black";
+        // bookElement styling
+        bookElement.style.border = "3px solid black";
         bookElement.style.padding = "5px";
         bookElement.style.width = "fit-content";
 
@@ -74,23 +78,34 @@ function displayBook() {
         bookElement.appendChild(authorElement);
         
         const pagesElement = document.createElement('p');
-        pagesElement.innerText = `pages: ${book.numOfPages}`;
+        pagesElement.innerText = `pages: ${book["number of pages"]}`;
         bookElement.appendChild(pagesElement);
 
         const readElement = document.createElement('p');
-        readElement.innerText = `read: ${book.readStatus}`;
+        readElement.innerText = `read: ${book["read status"]}`;
         bookElement.appendChild(readElement);
 
         bookPlaceholder.appendChild(bookElement);
     }
 }
 
-// add a button "new book"
+// add a "new book" button
 //     bring up a form to allow user to input information for
 //         book title
 //         book author
 //         number of pages
-//         read status 
+//         read status
+let newBookButton = document.querySelector('[data-new-book-button]');
+
+newBookButton.addEventListener('click', () => {
+    // console.log('hello');
+    addNewBook();
+})
+
+function addNewBook() {
+    let modal = document.querySelector('[data-modal]');
+    modal.showModal();
+};
 
 // add a button (to each book's display)
 //     remove book from library(array)
